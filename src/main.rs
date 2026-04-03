076a38b099c17dcbd3e7f2586928ecd623901de8
Onyx Mayer <osm@cmu.edu>
2026-04-03 14:48:58 -0400

    Updated source to read a file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use clap::Parser;
+
+
+#[derive(Parser)]
+struct Parameters {
+    #[arg(short, long)]
+    code_path: String,
+}
+
+
+fn main() -> io::Result<()> {
+	println!("Hello, world!");
+    let user_args = Parameters::parse(); 
+    let file = File::open(user_args.code_path)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?; // Handle potential I/O errors for each line
+        println!("{}", line);
+    }
+
+    Ok(())
 }
