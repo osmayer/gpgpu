d0d871f78121ec1194f0d316e3be013b6af201dd
Onyx Mayer <osm@cmu.edu>
2026-04-03 14:14:13 -0400

    Initial Commit

---
+++ gpgpu/target/debug/deps/gpgpu-8fa83a6ccd3adc40.d
@@ -0,0 +1,5 @@
+/Users/onyxmayer/cmu/classes/15418/gpgpu/gpgpu/target/debug/deps/gpgpu-8fa83a6ccd3adc40: src/main.rs
+
+/Users/onyxmayer/cmu/classes/15418/gpgpu/gpgpu/target/debug/deps/gpgpu-8fa83a6ccd3adc40.d: src/main.rs
+
+src/main.rs:
---
+++ gpgpu/target/debug/gpgpu.d
@@ -0,0 +1,1 @@
+/Users/onyxmayer/cmu/classes/15418/gpgpu/gpgpu/target/debug/gpgpu: /Users/onyxmayer/cmu/classes/15418/gpgpu/gpgpu/src/main.rs
