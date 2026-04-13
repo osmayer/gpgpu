24702ff2da753b46c49e751bce00318bc5dd39b0
Onyx Mayer <osm@cmu.edu>
2026-04-13 19:40:47 -0400

    Testing

---
+++ tests/simple/tid.disassembly.s
@@ -0,0 +1,18 @@
+
+simple/tid.elf:     file format elf32-littleriscv
+
+
+Disassembly of section .text:
+
+00400000 <main>:
+  400000:	1f400413          	addi	x8,x0,500
+  400004:	f3800493          	addi	x9,x0,-200
+  400008:	00840133          	add	x2,x8,x8
+  40000c:	009405b3          	add	x11,x8,x9
+  400010:	00848633          	add	x12,x9,x8
+  400014:	009486b3          	add	x13,x9,x9
+  400018:	00a00513          	addi	x10,x0,10
+  40001c:	0000068f          	.insn	4, 0x068f
+  400020:	0102468f          	.insn	4, 0x0102468f
+  400024:	00a50033          	add	x0,x10,x10
+  400028:	00000073          	ecall
---
+++ tests/simple/twoInstr.disassembly.s
@@ -0,0 +1,18 @@
+
+simple/twoInstr.elf:     file format elf32-littleriscv
+
+
+Disassembly of section .text:
+
+00400000 <main>:
+  400000:	1bf00293          	addi	x5,x0,447
+  400004:	00c5850f          	.insn	4, 0x00c5850f
+  400008:	00000013          	addi	x0,x0,0
+  40000c:	00000013          	addi	x0,x0,0
+  400010:	00000013          	addi	x0,x0,0
+  400014:	00000013          	addi	x0,x0,0
+  400018:	00000013          	addi	x0,x0,0
+  40001c:	00000013          	addi	x0,x0,0
+  400020:	00000013          	addi	x0,x0,0
+  400024:	00a00513          	addi	x10,x0,10
+  400028:	00000073          	ecall
